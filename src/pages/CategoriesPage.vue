7ced6e4c19fcfcffd9d211933a2910bfcc753c95
Gnarkill33 <gnarkill33@gmail.com>
2026-01-30 00:45:46 +0300

    feat: add basic layout of categories page

---
+++ src/pages/CategoriesPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue'
+import AppButton from '@/components/AppButton.vue'
+</script>
+
+<template>
+  <main>
+    <AppLayout>
+      <template #title> Categories </template>
+      <template #controls> <AppButton text="Add category" /> </template>
+      <template #inner> </template>
+    </AppLayout>
+  </main>
+</template>
