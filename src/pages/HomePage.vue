8ce966982008c92b64949a4b506cb9bdf0fc4aac
Gnarkill33 <gnarkill33@gmail.com>
2026-01-30 00:45:25 +0300

    feat: add fetching recipes to home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { RecipeService } from '@/services'
 import AppLayout from '@/layouts/AppLayout.vue'
 import AppButton from '@/components/AppButton.vue'
+
+const recipes = ref()
+
+const fetchRecipes = async () => {
+  try {
+    recipes.value = await RecipeService.getRecipesByLetter()
+  } catch {}
+}
+
+onMounted(fetchRecipes)
 </script>
 
 <template>
@@ -8,6 +20,7 @@ import AppButton from '@/components/AppButton.vue'
     <AppLayout>
       <template #title> Recipes </template>
       <template #controls> <AppButton text="Add recipe" /> </template>
+      <template #inner> {{ recipes }} </template>
     </AppLayout>
   </main>
 </template>
