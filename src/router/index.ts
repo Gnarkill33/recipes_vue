c02708e7616df4a8252026a367227d7b48b8050f
Gnarkill33 <gnarkill33@gmail.com>
2026-01-30 00:44:29 +0300

    feat: add router paths to constants

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,20 @@
 import HomePage from '@/pages/HomePage.vue'
+import CategoriesPage from '@/pages/CategoriesPage.vue'
+import { ROUTES } from '@/constants'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: ROUTES.HOME,
       name: 'home',
       component: HomePage,
+    },
+    {
+      path: ROUTES.CATEGORIES,
+      name: 'categories',
+      component: CategoriesPage,
     },
   ],
 })
