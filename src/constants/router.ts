11d4ae094fb8bf93f6f55e581da4128f894120a8
Gnarkill33 <gnarkill33@gmail.com>
2026-02-02 23:06:13 +0300

    feat: add function to get recipe by id on component mount

--- src/constants/router.ts
+++ src/constants/router.ts
@@ -1,4 +1,5 @@
 export const ROUTES = {
   HOME: '/',
   CATEGORIES: '/categories',
+  RECIPE: '/recipe/:id',
 }
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { RecipeService } from '@/services'
+import { ROUTES } from '@/constants'
 import AppLayout from '@/layouts/AppLayout.vue'
 import AppButton from '@/components/AppButton.vue'
 
@@ -9,7 +10,13 @@ const recipes = ref()
 const fetchRecipes = async () => {
   try {
     recipes.value = await RecipeService.getRecipesByLetter()
-  } catch {}
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const getRecipePath = (id: string) => {
+  return `${ROUTES.RECIPE.split(':')[0]}${id}`
 }
 
 onMounted(fetchRecipes)
@@ -20,7 +27,49 @@ onMounted(fetchRecipes)
     <AppLayout>
       <template #title> Recipes </template>
       <template #controls> <AppButton text="Add recipe" /> </template>
-      <template #inner> {{ recipes }} </template>
+      <template #inner>
+        <div class="wrapper">
+          <el-table :data="recipes" style="width: 100%">
+            <el-table-column prop="idMeal" label="#" />
+            <el-table-column label="Image">
+              <template #default="scope">
+                <router-link :to="getRecipePath(scope.row.idMeal)"
+                  ><img :src="scope.row.strMealThumb" class="img"
+                /></router-link>
+              </template>
+            </el-table-column>
+            <el-table-column prop="strArea" label="Area" />
+            <el-table-column prop="strCategory" label="Category" />
+            <el-table-column label="Tags">
+              <template #default="scope">
+                <template v-if="scope?.row?.strTags">
+                  <el-tag
+                    v-for="(tag, key) in scope.row.strTags.split(',')"
+                    :key="key"
+                    type="primary"
+                    class="tag"
+                  >
+                    {{ tag }}
+                  </el-tag>
+                </template>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </template>
     </AppLayout>
   </main>
 </template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/index.scss' as *;
+
+.tag {
+  margin: 2px 3px;
+}
+
+.img {
+  width: 70px;
+  height: 70px;
+}
+</style>
