b4d79032e630319c49e764907ae4c42ae62b5166
Gnarkill33 <gnarkill33@gmail.com>
2026-02-02 23:05:10 +0300

    feat: add new url and function to get specific recipe

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,3 @@
 export const URL_RECIPES_BY_LETTER = '/api/search.php?f=p'
+
+export const URL_RECIPES_BY_ID = '/api/lookup.php?i='
--- src/services/RecipeService/provider.ts
+++ src/services/RecipeService/provider.ts
@@ -1,8 +1,14 @@
 import axios from 'axios'
-import { URL_RECIPES_BY_LETTER } from '@/constants'
+import { URL_RECIPES_BY_LETTER, URL_RECIPES_BY_ID } from '@/constants'
 
 export const getRecipesByLetter = async () => {
   const data = await axios.get(URL_RECIPES_BY_LETTER)
 
-  return data
+  return data?.data.meals
+}
+
+export const getRecipesById = async (id: string) => {
+  const data = await axios.get(`${URL_RECIPES_BY_ID}${id}`)
+
+  return data?.data.meals[0]
 }
