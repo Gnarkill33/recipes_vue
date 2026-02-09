0c7fbd122fb2910b0c030f1ad45a2cdab309711b
Gnarkill33 <gnarkill33@gmail.com>
2026-02-09 23:49:49 +0300

    feat: add app loader

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,29 @@
+<template>
+  <transition>
+    <div class="root">
+      <div class="header">Loading...</div>
+    </div>
+  </transition>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/index.scss' as *;
+
+.root {
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba($white, 0.8);
+}
+
+.header {
+  font-size: 28px;
+  font-weight: 700;
+}
+</style>
